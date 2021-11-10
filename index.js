c12bdd6b9197bd367e058acb01ae475020bee9e0
Habownia <hubertkrz7@gmail.com>
2021-11-10 23:29:06 +0100

    funkcjonalność ikonki discord

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const people = document.querySelector('#people');
 const peoplePs = document.querySelector('#people-ps');
 
-const home = document.querySelector('#home');
-const homePs = document.querySelector('#home-ps');
-
-const teritory = document.querySelector('#teritory');
-const teritoryPs = document.querySelector('#teritory-ps');
-
 function peopleClick() {
 	peoplePs.classList.toggle('hidden');
 }
 people.addEventListener('click', peopleClick);
+
+const home = document.querySelector('#home');
+const homePs = document.querySelector('#home-ps');
 
 function homeClick() {
 	homePs.classList.toggle('hidden');
 }
 home.addEventListener('click', homeClick);
 
+const teritory = document.querySelector('#teritory');
+const teritoryPs = document.querySelector('#teritory-ps');
+
 function teritoryClick() {
 	teritoryPs.classList.toggle('hidden');
 }
 teritory.addEventListener('click', teritoryClick);
+
+const discord = document.querySelector('#discord');
+const discordPs = document.querySelector('#discord-ps');
+
+function discordClick() {
+	discordPs.classList.toggle('hidden');
+}
+discord.addEventListener('click', discordClick);
+
